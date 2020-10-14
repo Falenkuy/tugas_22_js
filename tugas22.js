c0e092a76b736d89086b98d5d42f190f2cb7e584
Valencinus Aten <valenaten22@gmail.com>
2020-10-14 14:26:58 +0700

    Tugas 22 js
    Tugas ini saya sampaikan ke tim Niomic

---
+++ tugas22.js
@@ -0,0 +1,7 @@
+function forEach(){
+  var kalimat = [ "Saya" , "ingin" , "belajar" , "bersama"]
+  kalimat.forEach(function(items,index){
+    console.log("Item : " + items + " Index ke " +index);
+  });
+}
+forEach()
